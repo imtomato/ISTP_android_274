4c32801fe05b198f20ed9ebfbd723b8179c6d5c2
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-09-05 19:28:34 +0800

    add loadListViewData function and construct BufferedReader object

--- app/src/main/java/com/example/user/istpandroidproject/model/OwnedPokemonInfoDataManager.java
+++ app/src/main/java/com/example/user/istpandroidproject/model/OwnedPokemonInfoDataManager.java
@@ -1,7 +1,46 @@
 package com.example.user.istpandroidproject.model;
 
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 /**
  * Created by user on 2016/9/5.
  */
 public class OwnedPokemonInfoDataManager {
+
+    Context mContext;
+    ArrayList<OwnedPokemonInfo> ownedPokemonInfos;
+
+    public OwnedPokemonInfoDataManager(Context context) {
+        mContext = context;
+    }
+
+    public void loadListViewData() {
+        ownedPokemonInfos = new ArrayList<>();
+
+        BufferedReader reader;
+        String line = null;
+        String[] dataFields = null;
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(
+                    mContext.getAssets().open("pokemon_data.csv")));
+
+            while((line = reader.readLine()) != null) {
+                dataFields = line.split(",");
+                //TODO: construct a object from dataFields
+            }
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
 }
