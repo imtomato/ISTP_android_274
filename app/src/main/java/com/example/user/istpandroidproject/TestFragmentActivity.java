a803a2eb4126f01bd598223bb9a93a7a5a1f31de
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-09-19 21:44:17 +0800

    add onClickListener

--- app/src/main/java/com/example/user/istpandroidproject/TestFragmentActivity.java
+++ app/src/main/java/com/example/user/istpandroidproject/TestFragmentActivity.java
@@ -2,12 +2,25 @@ package com.example.user.istpandroidproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TestFragmentActivity extends AppCompatActivity {
+public class TestFragmentActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_fragment);
+
+        Button f1Btn = (Button)findViewById(R.id.fragmentBtn1);
+        Button f2Btn = (Button)findViewById(R.id.fragmentBtn2);
+
+        f1Btn.setOnClickListener(this);
+        f2Btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
