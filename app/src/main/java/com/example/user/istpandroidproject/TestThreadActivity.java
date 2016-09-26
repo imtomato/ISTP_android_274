55525e349aaf796fb2de87521930b248795f7236
Kuan-Ting Chou <r03944003@ntu.edu.tw>
2016-09-26 22:05:13 +0800

    finish wifi event receiver

--- app/src/main/java/com/example/user/istpandroidproject/TestThreadActivity.java
+++ app/src/main/java/com/example/user/istpandroidproject/TestThreadActivity.java
@@ -1,9 +1,18 @@
 package com.example.user.istpandroidproject;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.example.user.istpandroidproject.model.Utils;
 
 import java.util.Calendar;
 
@@ -12,6 +21,9 @@ public class TestThreadActivity extends AppCompatActivity {
 
     Handler uiHandler;
     TextView timeInfoText;
+
+    Handler bgThreadHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +35,42 @@ public class TestThreadActivity extends AppCompatActivity {
         uiHandler = new Handler(getMainLooper());
         uiHandler.post(updateTimeInfoToUITask); //first execute update_time_task
 
+        HandlerThread bgThread = new HandlerThread("bgThread");
+        bgThread.start();
+        bgThreadHandler = new Handler(bgThread.getLooper());
+        bgThreadHandler.post(checkThreadIdTask);
+        Log.d("threadTest", "main thread id:" + Thread.currentThread().getId());
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(wifiEventReceiver, intentFilter);
+    }
+
+    BroadcastReceiver wifiEventReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String statusMsg = Utils.getConnectivityStatusString(context);
+            Log.d("wifiEvent", statusMsg);
+        }
+    };
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(wifiEventReceiver);
+    }
+
+    Runnable checkThreadIdTask = new Runnable() {
+        @Override
+        public void run() {
+            Log.d("threadTest", "thread id:" + Thread.currentThread().getId());
+        }
+    };
 
     int updateTimePeriodInSecs = 1;
 
--- app/src/main/java/com/example/user/istpandroidproject/model/Utils.java
+++ app/src/main/java/com/example/user/istpandroidproject/model/Utils.java
@@ -2,6 +2,8 @@ package com.example.user.istpandroidproject.model;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 
 import com.example.user.istpandroidproject.R;
@@ -21,6 +23,39 @@ public class Utils {
             return context.getResources().getDrawable(drawableId, null);
         }
 
+    }
+
+    public static int TYPE_WIFI = 1;
+    public static int TYPE_MOBILE = 2;
+    public static int TYPE_NOT_CONNECTED = 0;
+
+
+    public static int getConnectivityStatus(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (null != activeNetwork) {
+            if(activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                return TYPE_WIFI;
+
+            if(activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+                return TYPE_MOBILE;
+        }
+        return TYPE_NOT_CONNECTED;
+    }
+
+    public static String getConnectivityStatusString(Context context) {
+        int conn = getConnectivityStatus(context);
+        String status = null;
+        if (conn == TYPE_WIFI) {
+            status = "Wifi enabled";
+        } else if (conn == TYPE_MOBILE) {
+            status = "Mobile data enabled";
+        } else if (conn == TYPE_NOT_CONNECTED) {
+            status = "Not connected to Internet";
+        }
+        return status;
     }
 
 
