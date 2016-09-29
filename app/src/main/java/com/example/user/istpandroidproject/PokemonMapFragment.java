c93a8113297a685fbbdbc3e317ecbff70a107711
Wuteyan <killerw74@gmail.com>
2016-09-29 20:30:25 +0800

    add google api client

--- app/src/main/java/com/example/user/istpandroidproject/PokemonMapFragment.java
+++ app/src/main/java/com/example/user/istpandroidproject/PokemonMapFragment.java
@@ -7,7 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -15,9 +19,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PokemonMapFragment extends SupportMapFragment implements OnMapReadyCallback {
+public class PokemonMapFragment extends SupportMapFragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     GoogleMap googleMap;
+    GoogleApiClient googleApiClient;
     public PokemonMapFragment() {
         // Required empty public constructor
     }
@@ -40,6 +45,33 @@ public class PokemonMapFragment extends SupportMapFragment implements OnMapReady
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
-        
+        createGoogleApiClient();
+    }
+
+    public void createGoogleApiClient()
+    {
+        if(googleApiClient == null)
+        {
+            googleApiClient = new GoogleApiClient.Builder(getContext())
+                            .addConnectionCallbacks(this)
+                            .addOnConnectionFailedListener(this)
+                            .addApi(LocationServices.API)
+                            .build();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(getContext(), "Google Api Client Connection Suspended", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(getContext(), "Google Api Client Connection Failed", Toast.LENGTH_SHORT).show();
     }
 }
